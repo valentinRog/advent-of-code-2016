8d662f5802888a68f5509f6c81994ecabd7fe187
Valentin Rogister <valentin.rogister@gmail.com>
2023-04-21 14:50:31 +0200

    day12

---
+++ day12/part2.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func Part2(raw string) {
+	registers := map[string]int{
+		"a": 0,
+		"b": 0,
+		"c": 1,
+	}
+
+	instructions := map[string]func(arr []string) int{
+		"cpy": func(arr []string) int {
+			if v, ok := registers[arr[0]]; ok {
+				registers[arr[1]] = v
+			} else {
+				registers[arr[1]], _ = strconv.Atoi(arr[0])
+			}
+			return 1
+		},
+		"inc": func(arr []string) int {
+			registers[arr[0]]++
+			return 1
+		},
+		"dec": func(arr []string) int {
+			registers[arr[0]]--
+			return 1
+		},
+		"jnz": func(arr []string) int {
+			v, ok := registers[arr[0]]
+			if !ok {
+				v, _ = strconv.Atoi(arr[0])
+			}
+			if v != 0 {
+				j, _ := strconv.Atoi(arr[1])
+				return j
+			}
+			return 1
+		},
+	}
+
+	lines := strings.Split(raw, "\n")
+	for i := 0; i < len(lines); {
+		arr := strings.Fields(lines[i])
+		i += instructions[arr[0]](arr[1:])
+	}
+	fmt.Println(registers["a"])
+}
