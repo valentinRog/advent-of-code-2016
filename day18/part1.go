6b1a6d80a537b61e4dec27056bb79345d9fceeaf
Valentin Rogister <valentin.rogister@gmail.com>
2023-04-21 22:21:36 +0200

    day18

---
+++ day18/part1.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func Part1(raw string) {
+	traps := map[string]struct{}{
+		"^^.": {},
+		".^^": {},
+		"^..": {},
+		"..^": {},
+	}
+
+	getByte := func(s string) byte {
+		if _, ok := traps[s]; ok {
+			return '^'
+		}
+		return '.'
+	}
+
+	res := strings.Count(raw, ".")
+	for i := 0; i < 40-1; i++ {
+		var sb strings.Builder
+		sb.WriteByte(getByte("." + raw[:2]))
+		for i := 1; i < len(raw)-1; i++ {
+			sb.WriteByte(getByte(raw[i-1 : i+2]))
+		}
+		sb.WriteByte(getByte(raw[len(raw)-2:] + "."))
+		raw = sb.String()
+		res += strings.Count(raw, ".")
+	}
+	fmt.Println(res)
+}
