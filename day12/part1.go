ff5f06bd52418740efd0f526db50e1f459df25b2
Valentin Rogister <valentin.rogister@gmail.com>
2023-04-22 22:13:57 +0200

    using switch for day12 instead of map of functions

--- day12/part1.go
+++ day12/part1.go
@@ -12,41 +12,33 @@ func Part1(raw string) {
 		"b": 0,
 		"c": 0,
 	}
-
-	instructions := map[string]func(arr []string) int{
-		"cpy": func(arr []string) int {
-			if v, ok := registers[arr[0]]; ok {
-				registers[arr[1]] = v
+	lines := strings.Split(raw, "\n")
+	for i := 0; i < len(lines); i++ {
+		arr := strings.Fields(lines[i])
+		switch arr[0] {
+		case "cpy":
+			if v, ok := registers[arr[1]]; ok {
+				registers[arr[2]] = v
 			} else {
-				registers[arr[1]], _ = strconv.Atoi(arr[0])
+				registers[arr[2]], _ = strconv.Atoi(arr[1])
 			}
-			return 1
-		},
-		"inc": func(arr []string) int {
-			registers[arr[0]]++
-			return 1
-		},
-		"dec": func(arr []string) int {
-			registers[arr[0]]--
-			return 1
-		},
-		"jnz": func(arr []string) int {
-			v, ok := registers[arr[0]]
+		case "inc":
+			registers[arr[1]]++
+		case "dec":
+			registers[arr[1]]--
+		case "jnz":
+			v, ok := registers[arr[1]]
 			if !ok {
-				v, _ = strconv.Atoi(arr[0])
+				v, _ = strconv.Atoi(arr[1])
 			}
 			if v != 0 {
-				j, _ := strconv.Atoi(arr[1])
-				return j
+				v, ok = registers[arr[2]]
+				if !ok {
+					v, _ = strconv.Atoi(arr[2])
+				}
+				i += v - 1
 			}
-			return 1
-		},
-	}
-
-	lines := strings.Split(raw, "\n")
-	for i := 0; i < len(lines); {
-		arr := strings.Fields(lines[i])
-		i += instructions[arr[0]](arr[1:])
+		}
 	}
 	fmt.Println(registers["a"])
 }
